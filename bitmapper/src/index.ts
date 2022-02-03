da5059014aebb09eaec6370a103590c9fd35926a
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2022-02-03 15:17:23 +0530

    bitmapper init

---
+++ bitmapper/src/index.ts
@@ -0,0 +1,31 @@
+import path from "path";
+
+import { BitmapsGenerator, SVGHandler } from "./core";
+
+const root = path.resolve(__dirname, "../../");
+const svgDir = path.resolve(root, "svg");
+const themeName = "pokemon";
+
+const main = async () => {
+  console.log("Generating bitmaps for", themeName);
+
+  const bitmapsDir = path.resolve(root, "bitmaps", themeName);
+  const svg = new SVGHandler.SvgDirectoryParser(svgDir);
+
+  const png = new BitmapsGenerator(bitmapsDir);
+  const browser = await png.getBrowser();
+
+  for (let { key, content } of svg.getStatic()) {
+    console.log(" ==> Saving", key, "...");
+    await png.generateStatic(browser, content, key);
+  }
+
+  for (let { key, content } of svg.getAnimated()) {
+    console.log(" ==> Saving", key, "...");
+    await png.generateAnimated(browser, content, key);
+  }
+
+  await browser.close();
+};
+
+main();
